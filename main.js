92227ec792f96ecdb3dcb000a008191a856c405e
Max Brennemann <max@bizfactory.tech>
2025-11-25 18:41:48 +0100

    added new configs

--- main.js
+++ main.js
@@ -3,7 +3,43 @@ import { Pile } from "./js/Pile.js";
 import { Tile } from "./js/Tile.js";
 import { Path } from "./js/Path.js";
 
-window.game = {};
+const cards = {};
+const config = {
+    colors: 4,
+    columns: 10,
+    decks: 5,
+    numbers: ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
+    isEmptyAllowed: true,
+};
+
+function init() {
+    const cardContainer = document.getElementById("cardContainer");
+    const placeholders = document.getElementById("placeholders");
+    for (let i = 0; i < config.columns; i++) {
+        const column = document.createElement("div");
+        cardContainer.appendChild(column);
+
+        const placeholder = generatePlaceholder();
+        placeholders.appendChild(placeholder);
+    }
+}
+
+const generatePlaceholder = () => {
+    const template = document.getElementById("placeholderTemplate");
+    return template.content.cloneNode(true);
+}
+
+const generateEmpytCard = () => {
+
+}
+
+/**
+ * @param {*} number 
+ * @param {*} color (Herz: 1, Karo: 2, Peak: 3, Kreuz: 4)
+ */
+const generateCard = (number, color) => {
+
+}
 
 if (document.readyState !== 'loading' ) {
     init();
@@ -11,44 +47,4 @@ if (document.readyState !== 'loading' ) {
     document.addEventListener('DOMContentLoaded', function () {
         init();
     });
-}
-
-function init() {
-    const cols = document.querySelectorAll(".cards");
-    const template = document.querySelector("#cardTemplate");
-    const cardSolver = new Solver();
-    game.solver = cardSolver;
-
-    let iterations = 5;
-    for (let i = 1; i <= 10; i++) {
-        const anchor = document.querySelector(`#col-${i}`);
-
-        if (i > 4) {
-            iterations = 4;
-        }
-
-        const pile = new Pile();
-        cardSolver.addPile(pile);
-
-        for (let n = 0; n < iterations; n++) {
-            const container = template.content.cloneNode(true);
-            const title = container.querySelector(".card-title");
-            title.placeholder = "Karten";
-
-            const wrapper = document.createElement("div");
-            wrapper.appendChild(container);
-            anchor.appendChild(wrapper);
-
-            const tile = new Tile(wrapper);
-            pile.addTile(tile);
-        }
-    }
-}
-
-function clearCache() {
-
-}
-
-function solve() {
-    window.game.solver.solve();
 }
